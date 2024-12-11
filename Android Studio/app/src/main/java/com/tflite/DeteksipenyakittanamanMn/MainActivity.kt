d311f57ea494ee0dd327fd23ca6a31b6ede22952
Islahul Anwar <anwar.islahul@gmail.com>
2024-12-11 09:41:16 +0800

    customization of some code

--- Android Studio/app/src/main/java/com/tflite/DeteksipenyakittanamanMn/MainActivity.kt
+++ Android Studio/app/src/main/java/com/tflite/DeteksipenyakittanamanMn/MainActivity.kt
@@ -5,59 +5,45 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import com.tflite.DeteksipenyakittanamanMn.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private  lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_main)
         //memperkenalkan button yang sudah ditambahkan di layout activity_main.xml
-//        val btnPendeteksi: Button = findViewById(R.id.btn_move_detection)
-//        btnPendeteksi.setOnClickListener(this)
+        val btnPendeteksi: Button = findViewById(R.id.btn_move_detection)
         //menambahkan event onClick pada button btnMoveDetection
-        binding.btnMoveDetection.setOnClickListener(this)
-//
-//        val btnImporGaleri: Button = findViewById(R.id.btn_move_import)
-//        btnImporGaleri.setOnClickListener(this)
-        binding.btnMoveImport.setOnClickListener(this)
+        btnPendeteksi.setOnClickListener(this)
 
-//        val btnBantuan: Button = findViewById(R.id.btn_move_help)
-//        btnBantuan.setOnClickListener(this)
-            binding.btnMoveHelp.setOnClickListener(this)
+        val btnImporGaleri: Button = findViewById(R.id.btn_move_import)
+        btnImporGaleri.setOnClickListener(this)
 
-//        val btnTentang: Button = findViewById(R.id.btn_move_about)
-//        btnTentang.setOnClickListener(this)
-        binding.btnMoveHelp.setOnClickListener(this)
+        val btnBantuan: Button = findViewById(R.id.btn_move_help)
+        btnBantuan.setOnClickListener(this)
+
+        val btnTentang: Button = findViewById(R.id.btn_move_about)
+        btnTentang.setOnClickListener(this)
     }
     override fun onClick(v: View) {
         when (v.id) {
-//            R.id.btn_move_detection
-            binding.btnMoveDetection.id -> {
+            R.id.btn_move_detection -> {
                 //menambahkan suatu Intent pada method onClick()
                 val moveIntent = Intent(this@MainActivity, Pendeteksi::class.java)
                 startActivity(moveIntent)
             }
-//            R.id.btn_move_import
-            binding.btnMoveImport.id-> {
+            R.id.btn_move_import -> {
                 val moveIntent = Intent(this@MainActivity, DeteksiDariGaleri::class.java)
                 startActivity(moveIntent)
             }
-            //R.id.btn_move_help
-            binding.btnMoveHelp.id -> {
+            R.id.btn_move_help -> {
                 val moveIntent = Intent(this@MainActivity, Bantuan::class.java)
                 startActivity(moveIntent)
             }
-//            R.id.btn_move_about
-            binding.btnMoveAbout.id -> {
+            R.id.btn_move_about -> {
                 val moveIntent = Intent(this@MainActivity, Tentang::class.java)
                 startActivity(moveIntent)
             }
         }
     }
 }
-
